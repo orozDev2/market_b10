9b56749831f4c77eff4686774771166b12b5b973
Orozbek Zhenishbek uulu <oroz.jenishbek.2003@gmail.com>
2025-01-20 19:03:09 +0600

    CRUD | read | Work with serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,62 @@
 from rest_framework import serializers
 
-from store.models import Product
+from store.models import Product, ProductAttribute, Category, Tag, ProductImage
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductAttributeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ProductAttribute
+        exclude = ('product',)
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ProductImage
+        exclude = ('product',)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class TagSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+
+class ListProductSerializer(serializers.ModelSerializer):
+
+    # image1 = serializers.ImageField(source='image')
+    # category = serializers.CharField(source='category.name')
+    # tags = serializers.ListSerializer(child=serializers.CharField(source='tags.name'))
+    image = serializers.ImageField()
+    attributes = ProductAttributeSerializer(many=True)
+    category = CategorySerializer()
+    tags = TagSerializer(many=True)
+    images = ProductImageSerializer(many=True)
+
+    class Meta:
+        model = Product
+        exclude = ('content',)
+
+
+class DetailProductSerializer(serializers.ModelSerializer):
+
+    # image1 = serializers.ImageField(source='image')
+    # category = serializers.CharField(source='category.name')
+    # tags = serializers.ListSerializer(child=serializers.CharField(source='tags.name'))
+    image = serializers.ImageField()
+    attributes = ProductAttributeSerializer(many=True)
+    category = CategorySerializer()
+    tags = TagSerializer(many=True)
+    images = ProductImageSerializer(many=True)
 
     class Meta:
         model = Product
--- api/views.py
+++ api/views.py
@@ -5,19 +5,19 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 
-from api.serializers import ProductSerializer
+from api.serializers import ListProductSerializer, DetailProductSerializer
 from store.models import Product
 
 
 @api_view(['GET'])
 def list_products(request):
     products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
+    serializer = ListProductSerializer(products, many=True, context={'request': request})
     return Response(serializer.data)
 
 
 @api_view(['GET'])
 def detail_product(request, id):
     product = get_object_or_404(Product, id=id)
-    serializer = ProductSerializer(product)
+    serializer = DetailProductSerializer(product,  context={'request': request})
     return Response(serializer.data)
