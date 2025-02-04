b6f9ec596e36505ff30122e6d5264a2563c4cb0a
alfi7773 <lilittarave@gmail.com>
2025-02-04 16:46:38 +0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('product-images/', views.create_product_image),
     path('product-images/<int:id>/', views.delete_product_image),
     path('product-images/', views.delete_product_image),
-    path('product-attributes/', views.create_product_attr),
+    path('product-attributes/', views.CreateAttrApiView.as_view()),
     path('product-attributes/<int:id>/', views.update_delete_product_attr),
 ]
